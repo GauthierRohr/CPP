8462a0849eb828fbd2ecbe1cb3b2ad89cf71c0c5
Gauthier Rohr <grohr@c3r3s1.42lausanne.ch>
2025-08-22 16:09:00 +0200

    cast changed and CTRL + D treated

--- CPP_0/ex00/megaphone.cpp
+++ CPP_0/ex00/megaphone.cpp
@@ -13,28 +13,25 @@
 
 void ft_print_args(char **args)
 {
-	int i = 1;
-	while (args[i])
-	{
-		int j = 0;
-		while (args[i][j])
-		{
-			std::cout << (char)std::toupper(args[i][j]);
-			j++;
-		}
-		i++;
-	}
-	std::cout << std::endl; //saut de ligne
+    int i = 1;
+    while (args[i])
+    {
+        int j = 0;
+        while (args[i][j])
+        {
+            std::cout << static_cast<char>(std::toupper(args[i][j]));
+            j++;
+        }
+        i++;
+    }
+    std::cout << std::endl;
 }
 
-int	main (int ac, char **av)
+int main(int ac, char **av)
 {
-	if (ac == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
-	}
-	else
-		ft_print_args(av);
-	return (0);
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+        ft_print_args(av);
+    return 0;
 }
